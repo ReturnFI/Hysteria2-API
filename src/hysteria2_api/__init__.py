83f8179b549d609653a3e2816f8217fce0ce7a67
Whispering Wind <151555003+ReturnFI@users.noreply.github.com>
2025-06-10 13:24:37 +0330

    Update Version

--- src/hysteria2_api/__init__.py
+++ src/hysteria2_api/__init__.py
@@ -2,7 +2,7 @@ from .client import Hysteria2Client
 from .exceptions import Hysteria2Error, Hysteria2AuthError, Hysteria2ConnectionError
 from .models import TrafficStats, OnlineStatus
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __all__ = [
     "Hysteria2Client",
     "Hysteria2Error",
